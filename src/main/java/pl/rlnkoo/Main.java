58b9e464483dcd18f8deced007dbf38faf5d0f73
Artur Rolak <rolakartur96@gmail.com>
2024-11-19 19:31:00 +0100

    Project started

---
+++ src/main/java/pl/rlnkoo/Main.java
@@ -0,0 +1,13 @@
+package pl.rlnkoo;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
+}
